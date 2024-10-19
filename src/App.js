8f416171318080c3655abf920c7cf207e809851d
srip <sripathy788@gmail.com>
2024-10-19 19:26:29 +0530

    learning

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 //import {Example} from './Example';
+import { Student } from './Student';
+
 
 function App() {
   return (
     <>
     {/* <div>Hello this is example</div>
     <Example /> */}
+    <Student Name="xxx" Age="20" Course="Maths"/>
     </>
   );
 }
--- src/student.jsx
+++ src/student.jsx
@@ -1,18 +1,27 @@
 
-export const student=(props)=>{
-return(
-    <>
-    <table>
-<tbody>
-<tr> 
-<th></th>
-<td></td>
-</tr>
-
-</tbody>
-
-    </table>
-    </>
-);
-};
-//hi hello !!
+export const Student=(props)=>{
+    return(
+        <>
+        <div className="student">
+        <table>
+    <tbody>
+    <tr> 
+    <th>Name</th>
+    <td>{props.Name}</td>
+    </tr>
+    <tr> 
+    <th>Age</th>
+    <td>{props.Age}</td>
+    </tr>
+    <tr> 
+    <th>Course</th>
+    <td>{props.Course}</td>
+    </tr>
+    </tbody>
+    
+        </table>
+        </div>
+        </>
+    );
+    };
+    //hi hello !!
